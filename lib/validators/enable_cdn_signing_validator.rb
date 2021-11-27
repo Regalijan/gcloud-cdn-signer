364b3156f0f99747d428f3dbf473efb5916e7f50
Wolftallemo <72576136+Wolftallemo@users.noreply.github.com>
2021-11-27 03:06:13 -0500

    Remove secure media requirement to enable

--- lib/validators/enable_cdn_signing_validator.rb
+++ lib/validators/enable_cdn_signing_validator.rb
@@ -7,16 +7,11 @@ class EnableCDNURLSigningValidator
 
   def valid_value?(val)
     return true if val == 'f'
-    return false if !SiteSetting.secure_media?
     return false if SiteSetting.cdn_signed_urls_key.blank? or SiteSetting.cdn_signed_urls_key_name.blank?
     true
   end
 
   def error_message
-    if !SiteSetting.secure_media?
-      I18n.t('gcloud_cdn_signer.errors.no_secure_media')
-    end
-
     if SiteSetting.cdn_signed_urls_key.blank?
       I18n.t('gcloud_cdn_signer.errors.missing_signing_key')
     end
