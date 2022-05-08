9e728dcc1da7bed0c4bc58854edab2b9bc6f0605
Wolftallemo <72576136+Wolftallemo@users.noreply.github.com>
2022-05-08 18:23:36 -0400

    Return super if off instead of requiring reboot

--- plugin.rb
+++ plugin.rb
@@ -2,7 +2,7 @@
 
 # name: gcloud-cdn-signer
 # about: Plugin to create signed CDN URLs for Google Cloud
-# version: 0.1
+# version: 0.2
 # author: Wolftallemo
 # url: https://github.com/Wolftallemo/gcloud-cdn-signer
 
@@ -15,36 +15,38 @@ require_relative 'lib/validators/reset_cdn_signing_creds_validator.rb'
 enabled_site_setting :cdn_signed_urls_enabled
 
 after_initialize do
-  if SiteSetting.cdn_signed_urls_enabled? && !SiteSetting.s3_cdn_url.blank?
-    module DiscourseCDNSignedURLs    
-      def presigned_get_url(
-        url,
-        force_download: false,
-        filename: false,
-        expires_in: SiteSetting.cdn_signature_expiration_time
-      )
-        
-        if force_download && filename
-          opts = {
-            expires_in: S3Helper::DOWNLOAD_URL_EXPIRES_AFTER_SECONDS,
-            response_content_disposition: ActionDispatch::Http::ContentDisposition.format(
-              disposition: "attachment", filename: filename
-            )
-          }
-          obj = object_from_path(url)
-          return obj.presigned_url(:get, opts)
-        end
+  module DiscourseCDNSignedURLs
+    def presigned_get_url(
+      url,
+      force_download: false,
+      filename: false,
+      expires_in: SiteSetting.cdn_signature_expiration_time
+    )
 
-        signing_key = Base64.urlsafe_decode64 SiteSetting.cdn_signed_urls_key
-        unsigned_url = "#{SiteSetting.s3_cdn_url}/#{url}?Expires=#{Time.now.to_i + expires_in}&KeyName=#{SiteSetting.cdn_signed_urls_key_name}"
-        sig = OpenSSL::HMAC.digest "SHA1", signing_key, unsigned_url
-        encoded_sig = Base64.urlsafe_encode64 sig
-        "#{unsigned_url}&Signature=#{encoded_sig}"
+      if !SiteSetting.cdn_signed_urls_enabled? or SiteSetting.s3_cdn_url.blank?
+        return super
       end
-    end
 
-    class FileStore::S3Store
-      prepend DiscourseCDNSignedURLs
+      if force_download && filename
+        opts = {
+          expires_in: S3Helper::DOWNLOAD_URL_EXPIRES_AFTER_SECONDS,
+          response_content_disposition: ActionDispatch::Http::ContentDisposition.format(
+            disposition: "attachment", filename: filename
+          )
+        }
+        obj = object_from_path(url)
+        return obj.presigned_url(:get, opts)
+      end
+
+      signing_key = Base64.urlsafe_decode64 SiteSetting.cdn_signed_urls_key
+      unsigned_url = "#{SiteSetting.s3_cdn_url}/#{url}?Expires=#{Time.now.to_i + expires_in}&KeyName=#{SiteSetting.cdn_signed_urls_key_name}"
+      sig = OpenSSL::HMAC.digest "SHA1", signing_key, unsigned_url
+      encoded_sig = Base64.urlsafe_encode64 sig
+      "#{unsigned_url}&Signature=#{encoded_sig}"
     end
+  end
+
+  class FileStore::S3Store
+    prepend DiscourseCDNSignedURLs
   end
 end
